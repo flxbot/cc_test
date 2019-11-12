5bc5ee91b5123b8bcbfbd4c957c798d40205e36c
flxbot <flxbot@gmail.com>
2019-11-12 14:42:12 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var sass = require('gulp-sass');
 
 gulp.task('css', function() {
     var plugins = [];
-    return gulp.src('./_sass/config.scss')
+    return gulp.src('./csstest/config.scss')
         .pipe(postcss([
             uncss({
                 html: ['_site/**/*.html'],
